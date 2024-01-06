681c3da1617b215fdf2b79bd6865b4bb807c95df
ksmin23 <sonagy23@gmail.com>
2024-01-06 13:54:11 +0900

    initial commit

---
+++ cdk_stacks/app.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# vim: tabstop=2 shiftwidth=2 softtabstop=2 expandtab
+
+import os
+
+import aws_cdk as cdk
+
+from rag_with_aoss import (
+  VpcStack,
+  OpsServerlessVectorSearchStack,
+  SageMakerStudioStack,
+  EmbeddingEndpointStack,
+  LLMEndpointStack
+)
+
+APP_ENV = cdk.Environment(
+  account=os.environ["CDK_DEFAULT_ACCOUNT"],
+  region=os.environ["CDK_DEFAULT_REGION"]
+)
+
+app = cdk.App()
+
+vpc_stack = VpcStack(app, 'RAGVpcStack',
+  env=APP_ENV)
+
+sm_studio_stack = SageMakerStudioStack(app, 'RAGSageMakerStudioStack',
+  vpc_stack.vpc,
+  env=APP_ENV
+)
+sm_studio_stack.add_dependency(vpc_stack)
+
+ops_stack = OpsServerlessVectorSearchStack(app, 'RAGOpenSearchServerlessStack',
+  sm_studio_stack.sagemaker_execution_role_arn,
+  env=APP_ENV
+)
+ops_stack.add_dependency(sm_studio_stack)
+
+sm_embedding_endpoint = EmbeddingEndpointStack(app, 'EmbeddingEndpointStack',
+  sm_studio_stack.sagemaker_execution_role_arn,
+  env=APP_ENV
+)
+sm_embedding_endpoint.add_dependency(sm_studio_stack)
+
+sm_llm_endpoint = LLMEndpointStack(app, 'LLMEndpointStack',
+  sm_studio_stack.sagemaker_execution_role_arn,
+  env=APP_ENV
+)
+sm_llm_endpoint.add_dependency(sm_studio_stack)
+
+app.synth()
---
+++ cdk_stacks/rag_with_aoss/__init__.py
@@ -0,0 +1,5 @@
+from .vpc import VpcStack
+from .ops_serverless import OpsServerlessVectorSearchStack
+from .sm_studio import SageMakerStudioStack
+from .sm_embedding_endpoint import EmbeddingEndpointStack
+from .sm_llm_endpoint import LLMEndpointStack
